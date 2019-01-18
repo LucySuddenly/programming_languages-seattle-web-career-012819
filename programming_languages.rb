09f97136273fc0e49a4da8b4dcb84996421c3a12
Lucy Suddenly <suddenlylucy@gmail.com>
2019-01-18 18:44:35 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |name, type_info|
-      
+      type_info.each do |
     end 
   end
 end
